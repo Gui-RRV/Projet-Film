611074ab7bf587c548ca1a3da35dd055cbb769ec
Gui-RRV <rorive.guillaume@gmail.com>
2020-10-26 01:32:47 +0100

    Update bdd_connect.php

--- bdd_connect.php
+++ bdd_connect.php
@@ -1,5 +1,7 @@
 <?php
-$dbh = new PDO('mysql:host=db5000875227.hosting-data.io;dbname=dbs769208;charset=utf8', 
-               '', 
-               '');//comprenez que je cache les log car on ne sais jamais !
+
+$dbh = new PDO('mysql:host=localhost;dbname=netfloux;charset=utf8', 
+               'root', 
+               '');
+
 ?>
